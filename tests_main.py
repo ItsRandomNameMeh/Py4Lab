a5ed40bb7c89d304b73e30d4e038212f739ffeb6
macbook <d.enotow@yandex.ru>
2023-12-12 12:14:50 +0300

    refactored version

--- tests_main.py
+++ tests_main.py
@@ -3,13 +3,20 @@ from main import  sh_sort,  file_write
 
 
 class TestGenerate(unittest.TestCase):
-    def test_sort(self):
-        self.assertEqual(sh_sort([1,3,2]),[1,2,3])
-        self.assertEqual(sh_sort([1, 2, 3,4,5]), [1, 2, 3,4,5])
-        self.assertEqual(sh_sort([2,1]), [1,2])
-
-    def test_write(self):
-        self.assertRaises(ValueError, file_write, -33)
-        self.assertRaises(TypeError, file_write, "33")
-        self.assertRaises(TypeError, file_write, [123])
-        self.assertRaises(TypeError, file_write, False)
+    def setUp(self):
+        #
+        self.first_case_test = [1, 3, 2]
+        self.second_case_test = [1,2,3,4,5]
+        self.third_case_test = [2,1]
+        self.assertEqual(sh_sort(self.first_case_test),[1, 2, 3])
+        self.assertEqual(sh_sort(self.second_case_test), [1, 2, 3, 4, 5])
+        self.assertEqual(sh_sort(self.third_case_test), [1, 2])
+        #
+        self.int_error = -33
+        self.string_error = "33"
+        self.array_error = [123]
+        self.bool_error = False
+        self.assertRaises(ValueError, file_write, self.int_error)
+        self.assertRaises(TypeError, file_write, self.string_error)
+        self.assertRaises(TypeError, file_write, self.array_error)
+        self.assertRaises(TypeError, file_write, self.bool_error)
